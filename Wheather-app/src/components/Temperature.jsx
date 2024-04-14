7f90ed6c2f4664959105012784004a14d4af83f9
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-14 21:51:46 +0200

    skapat en input fält som ska användas för att söka runt

--- Wheather-app/src/components/Temperature.jsx
+++ Wheather-app/src/components/Temperature.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Temperature() {
   return (
-    <div>Temperature</div>
-  )
+    <>
+      <div className="flex justify-between">
+        <input
+          type="text"
+          className="bg-slate-600 border border-slate-500 text-slate-200 placeholder-slate-400 text-md w-60 p-2 focus:outline-none focus:border-slate-400"
+          placeholder="Search for your city"
+        />
+      </div>
+    </>
+  );
 }
 
-export default Temperature
+export default Temperature;
