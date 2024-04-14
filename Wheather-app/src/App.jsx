eb6c0126327ed855cc9d999f3311746762523175
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-14 21:44:21 +0200

    börjat skapandet av två container

--- Wheather-app/src/App.jsx
+++ Wheather-app/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import Temperature from "./components/Temperature";
 
 function App() {
+  return (
+    <div className="bg-[#1F213A] h-screen flex justify-center align-top">
+      <div className="bg-blue-200 mt-40 w-1/5 h-1/3">
+        <Temperature />
+        
+      </div>
 
-
- 
+      <div className="bg-blue-400 mt-40 w-1/3 h-1/3">Right</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
