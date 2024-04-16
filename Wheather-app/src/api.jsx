a8d876fe74b2d7eaf41508bb04b6b75cb4eae843
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-16 16:38:05 +0200

    gjort om min api fil, lagt till method och export av url/key, Så de går att användas i andra flikar

--- Wheather-app/src/api.jsx
+++ Wheather-app/src/api.jsx
@@ -1,28 +1,12 @@
-/*
-   Denna funktion utför en asynkron anrop för att hämta väderdata för en specifik stad.
-   Parametrar:
-   - city: En sträng som representerar namnet på staden för vilken väderdata ska hämtas.
+export const geoApioptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e346793e5dmsh27591f1b06c6022p14e0c0jsn031c8508b67a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
 
-   Funktionen bygger en API-url baserat på den angivna staden och utför sedan en fetch-förfrågan till API:et.
-   Om förfrågan är framgångsrik, returneras väderdatan i JSON-format.
-   Om förfrågan misslyckas, kastas ett fel och en felmeddelande skrivs ut i konsolen.
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-   Returnerar: En Promise som innehåller väderdatan för den angivna staden.
-*/
-
-export async function fetchWeatherData(city) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=b7caaa4e5ba0499daec150610241504&q=${city}&aqi=no`;
-  
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error("Error fetching data");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
-  }
-  
+export const WEATHER_API_URL = " https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2f7f55f67d48d9bfaa098b50fdfd6d07";
