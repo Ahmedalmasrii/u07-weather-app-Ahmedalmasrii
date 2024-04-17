91796f28b21be2b2097eb04134d09b03bbc6a511
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-17 04:03:05 +0200

    Visar daglig väderprognos i Accordion för de första 7 dagarna.

--- Wheather-app/src/components/forecast/forecast.jsx
+++ Wheather-app/src/components/forecast/forecast.jsx
@@ -30,16 +30,29 @@ const Forecast = ({ data }) => {
   const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
     WEEK_DAYS.slice(0, dayInAWeek)
   );
-
   return (
     <>
-    <label className="title">Daily</label>
+      <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
               <AccordionItemButton>
-                
+                <div className="dailty-item">
+                  <img
+                    src={`icons/${item.weather[0].icon}.png`}
+                    alt="weather"
+                    className="icon-small"
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}°C /
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
@@ -51,7 +64,6 @@ const Forecast = ({ data }) => {
     </>
   );
 };
-
 
 export default Forecast;
 
