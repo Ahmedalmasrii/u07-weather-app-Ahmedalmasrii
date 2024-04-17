1234a946bc3cefdc890d4e4556ae6cdc0158496e
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-17 05:48:16 +0200

    skapt funktion för att kunna söka i sökrutan med anrop till url

--- Wheather-app/src/components/search/search.jsx
+++ Wheather-app/src/components/search/search.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import PropTypes from "prop-types";
-
-
+import { GEO_API_URL, geoApioptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
+  };
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+
+      geoApioptions
+    )
+      .then((response) => response.json())
+
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude} `,
+              label: `${city.name}, ${city.countryCode}  `,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
   };
 
   return (
@@ -17,11 +36,12 @@ const Search = ({ onSearchChange }) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   );
 };
 Search.propTypes = {
-  onSearchChange: PropTypes.func.isRequired
+  onSearchChange: PropTypes.func.isRequired,
 };
 
 export default Search;
