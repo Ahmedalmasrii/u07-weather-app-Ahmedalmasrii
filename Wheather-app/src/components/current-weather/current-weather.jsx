bdecacd7613726d66c49498c8566e251dcd894db
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-17 04:30:34 +0200

    sen ska den rendera detaljer om vädret,stadens namn,ikon för väder,temp,känsla,vindhastighet,luftfuktighet och tryck

--- Wheather-app/src/components/current-weather/current-weather.jsx
+++ Wheather-app/src/components/current-weather/current-weather.jsx
@@ -14,7 +14,6 @@ const CurrentWeather = ({ data }) => {
           alt="weather"
           className="weather-icon"
         />
-        
       </div>
       <div className="bottom">
         {/* ska uppdatera temp baserat på inmatat värde/stad */}
