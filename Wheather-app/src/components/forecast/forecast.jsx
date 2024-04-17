e03ece4b43e8a8667cc89a6b6d5c8f5dcb0f55ef
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-17 03:55:28 +0200

    lagt till en struktru och logik, lagt till array med veckodagar

--- Wheather-app/src/components/forecast/forecast.jsx
+++ Wheather-app/src/components/forecast/forecast.jsx
@@ -1,11 +1,35 @@
-import { Accordion } from "react-accessible-accordion";
 
-const Forecast = () => {
+import PropTypes from "prop-types";
+import "./forecast.css";
+
+// En array som innehåller veckodagarna.
+const WEEK_DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+// En funktionell komponent som tar emot 'data' som props.
+
+const Forecast = ({ data }) => {
+      // Hämtar dagens index i veckodagsarrayen.
+
+  const dayInAWeek = new Date().getDay();
+    // Skapar en ny array som börjar med dagens veckodag och fortsätter till nästa vecka.
+
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
   return (
     <>
-      <label className="title">Daily</label>
-      <Accordion></Accordion>
+     
     </>
   );
 };
+
 export default Forecast;
