682e3ae3f22cdc3f63e823c218ef11d59eb9b76e
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-22 01:21:07 +0200

    skapat en funktion som omvandlar från celcius till fehrenhait vid intryk på knappen convert

--- Wheather-app/src/components/current-weather/current-weather.jsx
+++ Wheather-app/src/components/current-weather/current-weather.jsx
@@ -1,7 +1,16 @@
 import "./current-weather.css";
 import PropTypes from "prop-types";
 
-const CurrentWeather = ({ data }) => {
+const CurrentWeather = ({ data, handleConversion, isCelsius }) => {
+  const toggleUnit = () => {
+    // setIsCelsius(!isCelsius);
+    handleConversion(!isCelsius); // Skicka värdet av enheten till hanteringsfunktionen i App-komponenten
+  };
+
+  const convertTemperature = (temp) => {
+    return isCelsius ? temp : (temp * 9) / 5 + 32;
+  };
+
   return (
     <div className="weather">
       <div className="top">
@@ -17,27 +26,45 @@ const CurrentWeather = ({ data }) => {
       </div>
       <div className="bottom">
         {/* ska uppdatera temp baserat på inmatat värde/stad */}
-        <p className="temperature">{Math.round(data.main.temp)}°C</p> 
+        <p
+          className={
+            isCelsius ? "temperature celsius" : "temperature fahrenheit"
+          }
+        >
+          {isCelsius
+            ? `${Math.round(data.main.temp)}°C`
+            : `${Math.round((data.main.temp * 9) / 5 + 32)}°F`}
+        </p>
+        <button onClick={toggleUnit}>
+          {isCelsius ? "Convert to Fahrenheit" : "Convert to Celsius"}
+        </button>
+
         <div className="details">
-          <div className="parameter-row"> 
+          <div className="parameter-row">
             <span className="parameter-label">Details</span>
           </div>
-           {/* visar hur vädret känns */}
+
+          {/* visar hur vädret känns */}
+
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
+            <span className="parameter-value">
+              {Math.round(convertTemperature(data.main.feels_like))}
+              {isCelsius ? "°C" : "°F"}
+            </span>
           </div>
-             {/* visar det är vind eller inte */}
+
+          {/* visar det är vind eller inte */}
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
             <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
-             {/* visar humidityn i procent */}
+          {/* visar humidityn i procent */}
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
             <span className="parameter-value">{data.main.humidity}%</span>
           </div>
-             {/* visar  lufttryck  */}
+          {/* visar  lufttryck  */}
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
             <span className="parameter-value">{data.main.pressure} hpa</span>
@@ -48,23 +75,28 @@ const CurrentWeather = ({ data }) => {
   );
 };
 CurrentWeather.propTypes = {
-    data: PropTypes.shape({
-      city: PropTypes.string.isRequired,
-      weather: PropTypes.arrayOf(
-        PropTypes.shape({
-          description: PropTypes.string.isRequired,
-          icon: PropTypes.string.isRequired,
-        })
-      ).isRequired,
-      main: PropTypes.shape({
-        temp: PropTypes.number.isRequired,
-        feels_like: PropTypes.number.isRequired,
-        humidity:PropTypes.number.isRequired,
-        pressure:PropTypes.number.isRequired,
-      }).isRequired,
-      wind: PropTypes.shape({
-        speed: PropTypes.number.isRequired,
-      }).isRequired,
+  data: PropTypes.shape({
+    city: PropTypes.string.isRequired,
+
+    weather: PropTypes.arrayOf(
+      PropTypes.shape({
+        description: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    main: PropTypes.shape({
+      temp: PropTypes.number.isRequired,
+      feels_like: PropTypes.number.isRequired,
+      humidity: PropTypes.number.isRequired,
+      pressure: PropTypes.number.isRequired,
     }).isRequired,
-  };
+    wind: PropTypes.shape({
+      speed: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+
+  handleConversion: PropTypes.func.isRequired,
+  isCelsius: PropTypes.bool.isRequired,
+};
+
 export default CurrentWeather;
